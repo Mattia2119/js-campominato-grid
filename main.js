6030ab6b0f6df6fc7eecf7183c62ad3c5a84fddb
Mattia Bardino <Mattia.errol@gmail.com>
2022-02-01 17:33:23 +0100

    difficoltà

--- main.js
+++ main.js
@@ -1,18 +1,56 @@
 const grid = document.getElementById("grid");
 
-for (let i = 1; i<=64; i++) {
+//Creo gli elementi div, li inserisco nel documento e gli aggiungo la classe blue al click//
+
+for (let i = 1; i<=100; i++) {
 
     const node = document.createElement("div");
     
-    node.classList.add("quadrato");
+    node.classList.add("quadrato-facile");
 
     node.addEventListener("click", function (){
 
-      console.log(this);
-
-      this.classList.add("blue");
+      node.classList.add("blue");
 
     })
 
     grid.append(node);
 }
+
+const play = document.getElementById("play");
+
+
+
+play.addEventListener("click", function() {
+
+    document.getElementById("grid").innerHTML=""; 
+
+    let difficoltà = document.getElementById("difficoltà").value;
+    console.log(difficoltà);
+
+    livelloDifficoltà(difficoltà);
+}
+)
+
+function livelloDifficoltà() {
+    let quadrati = 0;
+    let ordine = "";
+    
+    if (difficoltà == "easy") {
+        quadrati = 100;
+        ordine = "dieci";
+    }else if (difficoltà == "hard") {
+        quadrati = 81;
+        ordine = "otto";
+    }else if (difficoltà =="impossible") {
+        quadrati = 49;
+        ordine = "sette";
+    } else {alert("Error, try again later...");
+
+    }
+
+    return [quadrati,ordine];
+
+}
+
+

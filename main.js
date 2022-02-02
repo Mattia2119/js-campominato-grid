91e5d2411726b77bb1630bb739c349500f417e97
Mattia Bardino <Mattia.errol@gmail.com>
2022-02-02 17:34:22 +0100

    push

--- main.js
+++ main.js
@@ -5,36 +5,45 @@ function play () {
 
     const difficoltà = document.getElementById("difficoltà").value;
 
-    let quadrati;
+    let numeroCelle;
     let quadratiPerRiga;
-    
-    if (difficoltà == "easy") {
-        quadrati = 100;
+        
+       if (difficoltà == "easy") {
+        numeroCelle = 100;
         quadratiPerRiga = 10;
-    } else if (difficoltà=="hard") {
-        quadrati = 81;
-        quadratiPerRiga = 9;
-    } else if (difficoltà == "impossible") {
-        quadrati= 49;
-        quadratiPerRiga = 7;
-    }
+        } else if (difficoltà =="hard") {
+            numeroCelle = 81;
+            quadratiPerRiga = 9;
+        } else if (difficoltà == "impossible") {
+            numeroCelle= 49;
+            quadratiPerRiga = 7;
+        }
+    
+        const numeroBombe = 16;
 
-    const grid = document.getElementById("grid");
+        const bombe = generaBombe(numeroBombe,numeroCelle);
+
+        console.log (bombe)
+
+
+
 
     //Effettuo il reset della griglia al click
+    
+    const grid = document.getElementById("grid");
 
     grid.innerHTML=""; 
 
-    for (let i = 1; i <= quadrati; i++) {
+    for (let i = 1; i <= numeroCelle; i++) {
         const nodo = document.createElement("div");
 
-        if (quadrati == 100) {
+        if (numeroCelle == 100) {
             nodo.classList.add("quadrato");
 
-        } else if (quadrati== 81) {
+        } else if (numeroCelle== 81) {
             nodo.classList.add("quadrato-medio");
             
-        } else if (quadrati == 49) {
+        } else if (numeroCelle == 49) {
             nodo.classList.add("quadrato-grande");
         }
 
@@ -44,11 +53,9 @@ function play () {
         
         grid.appendChild(nodo); 
     }
+
+    return true;
 }
-
-const numeroBombe = 16;
-
-generaBombe (numeroBombe, quadrati)
 
 
 
@@ -62,21 +69,29 @@ generaBombe (numeroBombe, quadrati)
 
 //FUNZIONI//
     
+//Funzione per gestire il click sulle celle
  function cellClick () {
     this.classList.add("blue");
     this.removeEventListener("click", cellClick);
 }
 
-function generaBombe (numeroBombe,quadrati) {
+//Funzione per generare le bombe
+function generaBombe (numeroBombe, numeroCelle) {
     
     const bombeGenerate = [];
 
     while (bombeGenerate.lenght < numeroBombe) {
-        bombeGenerate.push(randomIntFromInterval(1 , quadrati));
+        bombeGenerate.push(randomIntFromInterval(1 , numeroCelle));
     }
     return bombeGenerate;
 }
 
+//Funzione per generare un numero casuale compreso tra due valori
 function randomIntFromInterval(min, max) { 
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
+//Funzione per calcolare il numero delle celle
+function CalcoloCelle () {
+
+}
